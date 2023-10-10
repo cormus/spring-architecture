7a2135d19f0b637567bf4fee405563cd667b3ec2
Alex Ribeiro <alexprodutor@hotmail.com>
2023-10-10 19:59:59 -0300

    Implementado filtro de segurança verificar JWT

--- src/main/java/com/cormus/architecture/app/infra/security/SecurityTokenService.java
+++ src/main/java/com/cormus/architecture/app/infra/security/SecurityTokenService.java
@@ -3,6 +3,7 @@ package com.cormus.architecture.app.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.cormus.architecture.app.domain.entity.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -26,15 +27,28 @@ public class SecurityTokenService {
                     .withIssuer("API cormus.com")
                     .withSubject(usuario.getEmail())
                     .withClaim("nome", usuario.getNome())
-                    .withExpiresAt(dataExpiracaoToken())
+                    .withExpiresAt(_dataExpiracaoToken())
                     .sign(algorithm);
 
         } catch (JWTCreationException exception){
-            throw new RuntimeException("Erro ao tentar gerar o token JWT", exception);
+            throw new RuntimeException("Erro ao tentar gerar o token JWT.", exception);
         }
     }
 
-    private Instant dataExpiracaoToken() {
+    public String getTokenSubject(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API cormus.com")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado.", exception);
+        }
+    }
+
+    private Instant _dataExpiracaoToken() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 
